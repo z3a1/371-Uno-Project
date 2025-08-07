6e76b8ab0d84889ba8f624919d3d62f2cc0655a6
Jennifer Grinberg <jennifergrinberg@keysis.ca>
2025-08-07 16:05:22 -0700

    added queue so that tkinter can handle messages, but issues with handlerecv and start button not syncing up

--- server/client_ui.py
+++ server/client_ui.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox
 from clientState import ClientState
 from ui import *
+import queue
 
 
 HOST = '127.0.0.1'
@@ -25,6 +26,11 @@ class GUI:
         self.root.bind("<Button-1>",self.handleMenuBtn)
         # Fails because non blocking operation isn't completed, force it to fail on init
         # Then call it again when trying to join waiting lobby
+
+        self.msg_queue = queue.Queue()
+        threading.Thread(target=self.listen_to_server, daemon=True).start()
+        self.poll_server()
+
         self.root.mainloop()
 
         
@@ -32,11 +38,12 @@ class GUI:
         # if hasattr(event, 'widget') and event.widget.widgetName == 'button':
         #     return
         if self.clickState.joinWaitingRoom:
-            print(self.clientManager.playerObj["playerNum"])
-            self.clientManager.handleSend(action="JOIN GAME",data={})
-            print(self.clientManager.numOfPlayers)
-            self.clientManager.handleRecv()
-            waiting_room(self.root,self.clientManager.numOfPlayers, self.clickState)
+            #print(self.clientManager.playerObj["playerNum"])
+            #self.clientManager.handleSend(action="JOIN GAME",data={})
+            #print(self.clientManager.numOfPlayers)
+            #self.clientManager.handleRecv()
+            self.clientManager.handleSend(action="JOIN GAME", data={})
+            #waiting_room(self.root,self.clientManager.numOfPlayers, self.clickState)
         elif self.clickState.credits:
             print_credits(self.root,0,self.clickState)
         elif self.clickState.menu:
@@ -44,11 +51,14 @@ class GUI:
         elif self.clickState.instructions:
             print_instructions(self.root,0,self.clickState)
         elif self.clickState.startGame:
-            print(self.clientManager.playerObj["playerNum"])
-            self.clientManager.isGameRunning = True
-            self.clientManager.handleSend(action="START GAME",data={"playerNum": self.clientManager.playerObj["playerNum"]})
-            self.clientManager.handleRecv()
-            print(self.clientManager.lastPlayedCard)
+            self.clientManager.handleSend(action="START GAME", data={"playerNum": self.clientManager.playerObj["playerNum"]})
+            #print(self.clientManager.playerObj["playerNum"])
+            #self.clientManager.isGameRunning = True
+            #self.clientManager.handleSend(action="START GAME",data={"playerNum": self.clientManager.playerObj["playerNum"]})
+            #self.clientManager.handleRecv()
+            #print(self.clientManager.lastPlayedCard)
+
+            
             print_board(self.root, 
                         self.clientManager.playerObj,
                         self.clientManager.currentPlayerTurn, 
@@ -56,6 +66,7 @@ class GUI:
                         self.clientManager.otherPlayerCards, 
                         self.clientManager.numOfPlayers, 
                         self.clickState)
+            
         elif self.clickState.drawCard:
             pass
         else:
@@ -65,6 +76,51 @@ class GUI:
                     break
         self.clickState.reset()
 
+    def listen_to_server(self):
+        while True:
+            try:
+                data = self.clientManager.cSocket.recv(65535)
+                if not data:
+                    print("Server closed connection")
+                    break
+                message = pickle.loads(data)
+                self.msg_queue.put(message)
+            except Exception as e:
+                print("Error receiving from server:", e)
+                break    
+
+    def poll_server(self):
+        while not self.msg_queue.empty():
+            msg = self.msg_queue.get()
+            self.handle_server_message(msg)
+        self.root.after(100, self.poll_server) 
+
+    def handle_server_message(self, message):
+        if message.get("waitingRoom"):
+            player_num = message.get("playerNum")
+            self.clientManager.numOfPlayers = player_num
+            waiting_room(self.root, player_num, self.clickState)
+
+        elif message.get("startGame"):
+            self.clientManager.isGameRunning = True
+            print(self.clientManager.isGameRunning)
+            self.clientManager.playerObj["playerNum"] = message["playerNum"]
+            self.clientManager.lastPlayedCard = message["lastPlayedCard"]
+            self.clientManager.givenCards = message["playerCards"]
+            self.clientManager.currentPlayerTurn = message["currentPlayerTurn"]
+            print("currently printingf board for player", self.clientManager.playerNum)
+            print_board(self.root, 
+                self.clientManager.playerObj,
+                self.clientManager.currentPlayerTurn, 
+                self.clientManager.lastPlayedCard, 
+                self.clientManager.otherPlayerCards, 
+                self.clientManager.numOfPlayers, 
+                self.clickState)
+
+        elif message.get("winner"):
+            winner = message["winner"]
+            tk.messagebox.showinfo("Game Over", f"Player {winner} wins!")
 
 if __name__ == "__main__":
     GUI()
+    
